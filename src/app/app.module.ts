676bca84fda47e13b9bdc1ec52982a260e77dfba
Richard Yu <ryu323@gmail.com>
2017-07-27 20:13:13 -0700

    fix merge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,13 +18,9 @@ import { StudentSearchHeaderComponent } from './studentSearch/student-search-hea
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { ClassService } from './services/class/class.service'
-<<<<<<< HEAD
-import { ClassAPIService } from './services/class/class-api.service';
-=======
 import { ClassAPIService } from './services/class/class-api.service'
 import { StudentService } from './services/student/student.service'
 import { StudentAPIService } from './services/student/student-api.service'
->>>>>>> master
 
 import { ConfigService } from './config/config.service'
 
